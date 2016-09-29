b3ba57a74d68c8b7868abf0b7ffef7cde9eef11d
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-09-28 22:28:06 -0600

    refactored code

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
-//<-- state object with all the questions information -->
-
+// State object that keeps track of changes
 var state = {
-    question1:{
+    currentQuestionIndex:0,
+    score:0,
+}
+
+var QUESTIONS = [
+    {
         text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
         answers: [
             '0815',
@@ -12,8 +16,7 @@ var state = {
         ],
         correct: 0
     },
-    
-    question2:{
+    {
         text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
         answers: [
             '0815',
@@ -23,8 +26,7 @@ var state = {
         ],
         correct: 1
     },
-    
-    question3:{
+    {
         text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
         answers: [
             '0815',
@@ -34,8 +36,7 @@ var state = {
         ],
         correct: 2
     },
-    
-    question4:{
+    {
         text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
         answers: [
             '0815',
@@ -45,101 +46,101 @@ var state = {
         ],
         correct: 3
     }
- };
+];
 
-//<-- Functions to modify the state object -->
+// variables to obtain elements from html
+var questionsPageElement = $('.questions-page');
+var questionCurrentElement = $('.question-current');
+var questionsTotalElement = $('.questions-total');
+var questionElement = $('.question');
+var answersElement = $('.answers');
+var resultsPageElement = $('.results-page');
+var scoreElement = $('.score');
+var restartButtonElement = $('.restart-button');
 
-var presentQuestion = function(state,questionIndex){
-    var arrayQuestions = []
-    arrayQuestions.push(state.question1, state.question2, state.question3, state.question4)
-    window.currentQuestion = arrayQuestions[questionIndex]
+
+///// Game Logic /////
+
+// function for keeping track of current question
+var presentQuestion = function(state){
+    return QUESTIONS[state.currentQuestionIndex]
 }
 
-var quizPoints = function(state,choice){
-    var score = 0
-    if(window.currentQuestion.correct === choice){
-        score++
+// function for increasing the score
+var increaseScore = function(state){
+    state.score++
+    scoreElement.text(state.score)
+}
+
+// function for evaluating if user had the correct answer and to increase the question index
+var userAnswer = function(choice){
+    var question = presentQuestion(state)
+    if(question.correct === choice){
+        increaseScore(state)
     }
-    state["score"] = score
+    state.currentQuestionIndex++
 }
 
-//<-- Function to calculte total score -->
-
-var scorePoints = function(){
-    if(state.score === 1){
-        currentPoints = 1
-    }
-    else{
-        currentPoints = 0
-    }
-    totalPoints = totalPoints + currentPoints
-    currentPoints = 0  
+// function to specify where quiz ends
+var hasNextQuestion = function(state){
+    return (state.currentQuestionIndex < QUESTIONS.length)
 }
 
-//<-- Functions to render content in DOM -->
-
-var renderQuestions = function(element){
-    var questionHTML = "<p>" + window.currentQuestion.text + "</p>"
-    element.html(questionHTML)
+// function to start a new quiz
+var newGame = function(state){
+    state.currentQuestionIndex = 0
+    state.score = 0
 }
 
-var renderOptions = function(element){
-    var optionsHTML = window.currentQuestion.answers.map(function(item){
-        return "<li><button>" + item + "</button></li>"
-    })
-    element.html(optionsHTML)
-}
 
-var renderScore = function(state, element){
-    var scoreHTML = "<div>You scored <span class='score'>" + totalPoints + "</span>/<span class='questions-total'>4</span></div>" + "<button class='restart-button'>Restart</button>"
-    element.html(scoreHTML)
-}
+///// Display Logic /////
 
-// <--Global variables to keep track of questions and calculate points -->
-
-var counter = 0
-var totalPoints = 0
-var currentPoints = 0
-
-// <-- jQuery to make changes in DOM -->
-
-$(".answers").on("click","button",function(event){
-    event.preventDefault()
-    var choice = $(this).parent().index();
-    var questionIndex = counter + 1
-    $(".question-current").text(questionIndex + 1)  
-    if((questionIndex+1)<=4){
-        presentQuestion(state,questionIndex)
-        quizPoints(state,choice+1)
-        renderQuestions($('.question'))
-        renderOptions($(".answers"))
-        scorePoints()
-        counter++
-    }
-    else {
-        scorePoints()
-        $(".questions-page").hide()
-        $(".results-page").show()
-        renderScore(state, $(".results-page"))
-    }
-})
-
-$(".results-page").on("click","button",function(){
-    $(".questions-page").show()
-    $(".results-page").hide()
-    counter=0
-    presentQuestion(state,counter)
-    renderQuestions($('.question'))
-    renderOptions($(".answers"))
-    $(".question-current").text(1)
-    totalPoints = 0
-})
-
+// jQuery to start the quiz
 $(document).ready(function(){
-    presentQuestion(state,counter)
-    renderQuestions($('.question'))
-    renderOptions($(".answers"))
+    renderQuestions(state)
+    questionsTotalElement.text(QUESTIONS.length)
 });
+
+// function to display the questions
+var renderQuestions = function(state){
+    var question = presentQuestion(state)
+    questionCurrentElement.text(state.currentQuestionIndex)
+    questionElement.text(question.text)
+    answersElement.empty()
+    for (var i = 0; i < question.answers.length; i++){
+        var answer = question.answers[i]
+        answersElement.append("<li><button type='button'>" + answer + "</button></li>")
+    }
+}
+
+// functions to show or hide the questions and the results
+var showResult = function(){
+    questionsPageElement.hide()
+    resultsPageElement.show()
+}
+
+var showQuestions = function(){
+    questionsPageElement.show()
+    resultsPageElement.hide()
+}
+
+///// Event Handlers /////
+
+// event handler to obtain user choice, evaluate it, and when the quiz ends show the results
+answersElement.on("click","button",function(){
+    var choice = $(this).parent().index();
+    userAnswer(choice)
+    hasNextQuestion(state)? renderQuestions(state) : showResult()
+})
+
+// event handler to start a new game after clickin restart button
+restartButtonElement.click(function(){
+    scoreElement.text(0)
+    newGame(state)
+    renderQuestions(state)
+    showQuestions()
+})
+
 
 
 
